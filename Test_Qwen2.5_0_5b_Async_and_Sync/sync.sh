04c1e237d47c280644d2cade269371ed8eed5bfb
shengtao <107896819+Wentcode@users.noreply.github.com>
2026-01-04 17:38:58 +0800

    update

---
+++ Test_Qwen2.5_0_5b_Async_and_Sync/sync.sh
@@ -0,0 +1,51 @@
+set -x
+
+export VLLM_ASCEND_ENABLE_NZ=0
+export VLLM_ATTENTION_BACKEND=XFORMERS
+
+# 定义日志文件名，建议带上时间戳防止覆盖
+LOG_FILE="/home/ma-user/work/training_log_$(date +%Y%m%d_%H%M%S).txt"
+echo "Logs will be saved to: ${LOG_FILE}"
+
+# 使用 2>&1 | tee 将标准输出和错误输出同时写入文件和屏幕
+python3 -m verl.trainer.main_ppo \
+    algorithm.adv_estimator=grpo \
+    data.train_files=/home/ma-user/work/data/gsm8k/train.parquet \
+    data.val_files=/home/ma-user/work/data/gsm8k/test.parquet \
+    data.train_batch_size=128 \
+    data.max_prompt_length=2048 \
+    data.max_response_length=3072 \
+    data.filter_overlong_prompts=True \
+    data.truncation='error' \
+    actor_rollout_ref.model.path=/home/ma-user/work/models/Qwen2.5-0.5B-Instruct \
+    actor_rollout_ref.actor.optim.lr=5e-7 \
+    actor_rollout_ref.model.use_remove_padding=False \
+    actor_rollout_ref.actor.entropy_coeff=0.001 \
+    actor_rollout_ref.actor.ppo_mini_batch_size=64 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=2 \
+    actor_rollout_ref.actor.use_kl_loss=True \
+    actor_rollout_ref.actor.kl_loss_coef=0.001 \
+    actor_rollout_ref.actor.kl_loss_type=low_var_kl \
+    actor_rollout_ref.model.enable_gradient_checkpointing=True \
+    actor_rollout_ref.actor.fsdp_config.param_offload=False \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=2 \
+    actor_rollout_ref.rollout.enable_chunked_prefill=False \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
+    actor_rollout_ref.rollout.n=8 \
+    actor_rollout_ref.rollout.name=vllm \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.7 \
+    actor_rollout_ref.rollout.n=8 \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=2 \
+    actor_rollout_ref.ref.fsdp_config.param_offload=True \
+    algorithm.kl_ctrl.kl_coef=0.001 \
+    trainer.critic_warmup=0 \
+    trainer.logger=console \
+    trainer.project_name='verl_grpo_example_gsm8k' \
+    trainer.experiment_name='qwen2_7b_function_rm' \
+    trainer.n_gpus_per_node=2 \
+    trainer.nnodes=1 \
+    trainer.save_freq=-1 \
+    trainer.test_freq=5 \
+    trainer.total_epochs=1 \
+    trainer.device=npu $@ 2>&1 | tee ${LOG_FILE}
